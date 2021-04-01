4d036cd3c1fbe308c9a3b82a18ea6f9c5576babd
Wes Young <wesley.young@du.edu>
2021-04-01 12:56:50 -0600

    Added functionality to contact form

--- src/components/ContactSection/ContactSection.js
+++ src/components/ContactSection/ContactSection.js
@@ -1,44 +1,58 @@
 import React from "react";
 import "./style.css";
+import emailjs from "emailjs-com";
 
 //Returns contact form
 const ContactSection = () => {
+  const emailSubmit = (event) => {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        `${process.env.REACT_APP_SERVICE_ID}`,
+        `${process.env.REACT_APP_TEMPLATE_ID}`,
+        event.target,
+        `${process.env.REACT_APP_USER_ID}`
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (err) => console.log(err.text)
+      );
+    event.target.reset();
+  };
+
   return (
     <section className="contactSection">
-      <form className="contactForm">
-        <div>
-          <label htmlFor="userName">Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="userName"
-            placeholder="Enter Name"
-          />
-        </div>
-        <div>
-          <label htmlFor="userEmail">Email</label>
-          <input
-            type="text"
-            className="form-control"
-            id="userEmail"
-            placeholder="Email Address"
-          />
-        </div>
-        <div>
-          <label htmlFor="userMessage">Message</label>
-          <textarea
-            type="text"
-            className="form-control"
-            id="userMessage"
-            placeholder="Message"
-            rows="3"
-          ></textarea>
-        </div>
-        <div>
-          <button type="button" className="btn btn-dark">
-            Submit
-          </button>
-        </div>
+      <form className="contactForm" onSubmit={emailSubmit}>
+        <label htmlFor="userName">Name</label>
+        <input
+          name="user"
+          type="text"
+          className="form-control"
+          id="userName"
+          placeholder="Enter Name"
+        />
+        <label htmlFor="userEmail">Email</label>
+        <input
+          name="user_email"
+          type="text"
+          className="form-control"
+          id="userEmail"
+          placeholder="Email Address"
+        />
+        <label htmlFor="userMessage">Message</label>
+        <textarea
+          name="user_message"
+          type="text"
+          className="form-control"
+          id="userMessage"
+          placeholder="Message"
+          rows="3"
+        ></textarea>
+        <button type="submit" className="btn btn-dark">
+          Submit
+        </button>
       </form>
       <aside className="contactInfo">
         <h4 className="contactDetails">Wesley Young</h4>
