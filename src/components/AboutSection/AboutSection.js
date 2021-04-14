84e045abdeaeb8fe793afc794b49632e34308311
Wes Young <wesley.young@du.edu>
2021-04-14 14:37:03 -0600

    AFixed mobile response for cards

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -6,23 +6,25 @@ import "./style.css";
 const AboutSection = () => {
   return (
     <section className="aboutSection">
-      <img className="profilePic" src={ProfilePic} alt="profilepic"></img>
-      <aside className="bio">
-        <p>
-          My name is Wesley Young. I am based in Castle Rock, Colorado. I have
-          bachelor's degree in mechanical engineering from Western Michigan
-          University and completed a for Full Stack Web Development Bootcamp at
-          the University of Denver. I am a problem solver and love to solve
-          puzzles. During my journey to become an engineer, I found a passion
-          for creating and troubleshooting code.
-        </p>
-        <p>
-          <b>Languages:</b> JavaScript, CCS, HTML, SQL, & NoSQL
-        </p>
-        <p>
-          <b>Technologies:</b> Express, Node.js, jQuery, MySQL, & MongoDB
-        </p>
-      </aside>
+      <div className="aboutChildren">
+        <img className="profilePic" src={ProfilePic} alt="profilepic"></img>
+        <aside className="bio">
+          <p>
+            My name is Wesley Young. I am based in Castle Rock, Colorado. I have
+            bachelor's degree in mechanical engineering from Western Michigan
+            University and completed a for Full Stack Web Development Bootcamp
+            at the University of Denver. I am a problem solver and love to solve
+            puzzles. During my journey to become an engineer, I found a passion
+            for creating and troubleshooting code.
+          </p>
+          <p>
+            <b>Languages:</b> JavaScript, CCS, HTML, SQL, & NoSQL
+          </p>
+          <p>
+            <b>Technologies:</b> Express, Node.js, jQuery, MySQL, & MongoDB
+          </p>
+        </aside>
+      </div>
     </section>
   );
 };
