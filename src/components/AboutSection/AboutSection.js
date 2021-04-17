0fa502283f5615ddc69affd2a4696a01e8613de6
Wes Young <wesley.young@du.edu>
2021-04-17 10:28:19 -0600

    Updated text in about

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -12,10 +12,10 @@ const AboutSection = () => {
           <p>
             My name is Wesley Young. I am based in Castle Rock, Colorado. I have
             bachelor's degree in mechanical engineering from Western Michigan
-            University and completed a for Full Stack Web Development Bootcamp
-            at the University of Denver. I am a problem solver and love to solve
+            University and completed a Full Stack Web Development Bootcamp at
+            the University of Denver. I am a problem solver and love to solve
             puzzles. During my journey to become an engineer, I found a passion
-            for creating and troubleshooting code.
+            for writing and troubleshooting code.
           </p>
           <p>
             <b>Languages:</b> JavaScript, CCS, HTML, SQL, & NoSQL
